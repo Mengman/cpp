9c9e59e23c8a8828199c7fecf71a1101e3dc2218
ycli <yucai.li@foxmail.com>
2015-05-20 18:49:56 +0800

    chapter 8 practice 3

--- chapter_8/p_3.cpp
+++ chapter_8/p_3.cpp
@@ -13,23 +13,27 @@ int main()
   getline(cin, input);
   while(input != "q")
   {
-      convert_up_case(input);
-      cout << input << endl;
+    convert_up_case(input);
 
       cout << "Enter a string (q to quit): ";
       getline(cin, input);
   }
+  if (input == "q")
+  {
+    cout << "Bye.\n";
+  }
 
   return 0;
-
 }
 
 void convert_up_case(string & s)
 {
-  char * temp = s.data();
-  while(temp != '\0')
+  const char * char_pointer = s.c_str();
+  while(* char_pointer != '\0')
   {
-
+    cout << (char) toupper(* char_pointer);;
+    char_pointer++;
   }
-  s = toupper(s);
+
+  cout << '\n';
 }
